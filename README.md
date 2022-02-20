f19db30921b1b63d1f38383960be9ca440ddd43a
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2022-02-20 18:03:08 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BYAlertManager
+
+A description of this package.
---
+++ Sources/BYAlertManager/BYAlertManager.swift
@@ -0,0 +1,6 @@
+public struct BYAlertManager {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
